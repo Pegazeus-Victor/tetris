5f50da29082072f89035819e55d159f167f67a77
Pegazeus-Victor <61429368+Pegazeus-Victor@users.noreply.github.com>
2020-05-12 13:10:59 -0700

    Updates

---
+++ precache-manifest.c4aa60ff2a43f4083cfba04c1e5ae106.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f675d11e6bf52a613ca408b0abcb342",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "07dd4a074a57c0a7ef8a",
+    "url": "/tetris/static/css/main.ad922669.chunk.css"
+  },
+  {
+    "revision": "d17c98fab453703c9ca1",
+    "url": "/tetris/static/js/2.e6d39061.chunk.js"
+  },
+  {
+    "revision": "07dd4a074a57c0a7ef8a",
+    "url": "/tetris/static/js/main.09bb921d.chunk.js"
+  },
+  {
+    "revision": "e9e58439d3b8c64431c2",
+    "url": "/tetris/static/js/runtime~main.61707ba2.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "a7e88d85362b0e07bc9278d39c1cc2f2",
+    "url": "/tetris/static/media/matrix.a7e88d85.jpg"
+  }
+]);
